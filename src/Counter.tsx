a59c4a96a19d65a9441fe4e215e7a464f904b808
Randolph <86254950+RR2894@users.noreply.github.com>
2024-04-06 17:33:58 +0300

    added UI with images

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,16 +1,40 @@
+import { useState } from "react"
+import attack from './assets/attack.png'
+import defend from './assets/defend.png'
+
 const Counter = () => {
+
+    const [counter, setCounter] = useState(0)
+
     const handleAttack = () => {
-        alert('Attack is clicked!')
+        // alert('Attack is clicked!')
+        setCounter(counter +1)
     }
     const handleDefense = () => {
-        alert('Defend is clicked!')
+        // alert('Defend is clicked!')
+        setCounter(counter -1)
     }
 
   return (
-    <div className="row text-white">
-        <h1>Counter: </h1>
+    <div className="row text-white text-center">
+        <h1>Counter: {counter} </h1>
+        <p>You win at +10, and you lose at -10 points</p>
+        <p>Let's play!</p>
+        <p>Game status</p>
+        <div className='col-6 col-md-3 offset-md-3'>
         <button onClick={handleAttack} style={{width: '200px'}}>+1</button>
+            <img src={attack} alt="attack" onClick={handleAttack} style={{cursor:'pointer', width:'100%', border:'2px solid green'}}/>
+        </div>
+        <div className='col-6 col-md-3 offset-md-3'>
         <button onClick={handleDefense} style={{width: '200px'}}>-1</button>
+        <img src={defend} alt="defend" onClick={handleDefense} style={{cursor:'pointer', width:'100%', border:'2px solid green'}}/>
+        </div>
+
+        <div className='col-12 col-md-4 offset-md-4'>
+            <button className='btn btn-secondary w-100 mt-2' >Random play</button>
+            <button className='btn btn-secondary w-100 mt-2' >Reset</button>
+        </div>
+
     </div>
    )
 }
