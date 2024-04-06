488e16b007bc99de14907aca095b3403ad256184
Randolph <86254950+RR2894@users.noreply.github.com>
2024-04-06 16:47:53 +0300

    added buttons

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,7 +1,18 @@
 const Counter = () => {
+    const handleAttack = () => {
+        alert('Attack is clicked!')
+    }
+    const handleDefense = () => {
+        alert('Defend is clicked!')
+    }
+
   return (
-    <div>Counter</div>
-  )
+    <div className="row text-white">
+        <h1>Counter: </h1>
+        <button onClick={handleAttack} style={{width: '200px'}}>+1</button>
+        <button onClick={handleDefense} style={{width: '200px'}}>-1</button>
+    </div>
+   )
 }
 
 export default Counter
