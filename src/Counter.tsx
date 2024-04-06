74d1ba56eea6c7237683eaad516666e94a4140b7
Randolph <86254950+RR2894@users.noreply.github.com>
2024-04-06 18:07:33 +0300

    added status and last play

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,71 +1,94 @@
-import { useState } from "react"
-import attack from './assets/attack.png'
-import defend from './assets/defend.png'
+import { useState } from 'react';
+import attack from './assets/attack.png';
+import defend from './assets/defend.png';
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const [gameStatus, setGameStatus] = useState('');
+  const [lastPlay, setLastPlay] = useState('');
 
-    const [counter, setCounter] = useState(0)
+  const handleAttack = () => {
+    // alert('Attack Clicked!');
+    setCounter((preCounter) => {
+      let newCount = preCounter + Math.round(Math.random() * 10);
+      setLastPlay('Attacked');
 
-    const handleAttack = () => {
-        // alert('Attack is clicked!')
-        setCounter((preCounter) => {
-            let newCount = preCounter + Math.round(Math.random() * 10)
-            return newCount
-        })
-        }
-        const handleDefense = () => {
-            // alert('Defend is clicked!')
-        setCounter((preCounter) => {
-            let newCount = preCounter + Math.round(Math.random() * 10)
-            return newCount
-            })
-    }
+      //   if (newCount > 10) {
+      //     setGameStatus('You Won!');
+      //   }
 
-const handleReset = () => {
-    setCounter(0)
-}
+      newCount > 10 ? setGameStatus('You Won!') : setGameStatus('');
 
-const handleRandomPlay = () => {
-    let playMode = Math.round(Math.random())
-    if(playMode === 0){
-        handleAttack()
+      return newCount;
+    });
+  };
+  const handleDefence = () => {
+    // alert('Defend Clicked!');
+    // setCounter(counter - 1);
+    setCounter((preCounter) => {
+      let newCount = preCounter - Math.round(Math.random() * 10);
+      setLastPlay('Defended');
+      //   if (newCount < -10) {
+      //     setGameStatus('You Lose!');
+      //   }
+
+      newCount < -10 ? setGameStatus('You Lose!') : setGameStatus('');
+
+      return newCount;
+    });
+  };
+
+  const handleReset = () => {
+    setCounter(0);
+  };
+
+  const handleRandomPlay = () => {
+    let playMode = Math.round(Math.random());
+    if (playMode === 0) {
+      handleAttack();
     } else {
-        handleDefense()
+      handleDefence();
     }
-}
+
+    // ternary operator
+    // playMode === 0 ? handleAttack() : handleDefence;
+  };
 
   return (
-    <div className="row text-white text-center">
-        <h1>Counter: {counter} </h1>
-        <p>You win at +10, and you lose at -10 points</p>
-        <p>Let's play!</p>
-        <p>Game status</p>
-        <div className='col-6 col-md-3 offset-md-3'>
-        <button onClick={handleAttack} style={{width: '200px'}}>+1</button>
-            <img 
-            src={attack} 
-            alt="attack" 
-            onClick={handleAttack} 
-            style={{cursor:'pointer', width:'100%', border:'2px solid green'}}/>
-        </div>
-        <div className='col-6 col-md-3 offset-md-3'>
-        <button onClick={handleDefense} style={{width: '200px'}}>-1</button>
-            <img 
-            src={defend} 
-            alt="defend" 
-            onClick={handleDefense} 
-            style={{cursor:'pointer', 
-            width:'100%', 
-            border:'2px solid green'}}/>
-        </div>
+    <div className='row text-white text-center'>
+      <h1>Counter: {counter} </h1>
+      <p>You win at +10 points and lose at -10 points</p>
+      <p>Let's Play: {lastPlay} </p>
+      <p>Game status: {gameStatus}</p>
+      <div className='col-6 col-md-3 offset-md-3'>
+        <img
+          className='p-4 rounded'
+          src={attack}
+          alt='Attack'
+          onClick={handleAttack}
+          style={{ cursor: 'pointer', width: '100%', border: '2px solid green' }}
+        />
+      </div>
+      <div className='col-6 col-md-3'>
+        <img
+          className='p-4 rounded'
+          src={defend}
+          alt='Defend'
+          onClick={handleDefence}
+          style={{ cursor: 'pointer', width: '100%', border: '2px solid green' }}
+        />
+      </div>
 
-        <div className='col-12 col-md-4 offset-md-4'>
-            <button onClick={handleRandomPlay} className='btn btn-secondary w-100 mt-2' >Random play</button>
-            <button onClick={handleReset} className='btn btn-secondary w-100 mt-2' >Reset</button>
-        </div>
-
+      <div className='col-12 col-md-4 offset-md-4'>
+        <button onClick={handleRandomPlay} className='btn btn-secondary w-100 mt-2'>
+          Random Play
+        </button>
+        <button onClick={handleReset} className='btn btn-warning w-100 mt-2'>
+          Reset
+        </button>
+      </div>
     </div>
-   )
-}
+  );
+};
 
-export default Counter
+export default Counter;
